6ef642717255d76f51f93cbb521458a3812de74a
Zitata <e.zitata@gmail.com>
2025-04-01 19:13:43 +0300

    чистка

---
+++ src/additional/s21_print_bits.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+
+void s21_print_bits(s21_decimal value) {
+    for (int i = 0; i < 4; i++) {
+        for (int i2 = 31; i2 >= 0; i2--) {
+            printf("%d", value.bits[i] >> i2 & 1);
+            if (i2 % 8 == 0) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
